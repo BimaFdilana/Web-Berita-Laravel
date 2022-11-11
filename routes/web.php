6590a023d7811be40cdfe19df9ba764cbe2cf1ab
BimaFadiLana <bima.fadilana01@gmail.com>
2022-11-11 10:20:22 +0700

    Project awal

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\authController;
+use App\Http\Controllers\experienceController;
+use App\Http\Controllers\halamanController;
+use App\Models\halaman;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('home', 'dashboard');
+
+
+Route::get('/auth', [authController::class, "index"])->name('login');
+Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('guest');
+Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest');
+Route::get('/auth/logout', [authController::class, "logout"]);
+
+
+Route::prefix('dashboard')->middleware('auth')->group(
+    function () {
+        Route::get('/', [halamanController::class, 'index']);
+        Route::resource('halaman', halamanController::class);
+        Route::resource('experience', experienceController::class);
+    }
+);
